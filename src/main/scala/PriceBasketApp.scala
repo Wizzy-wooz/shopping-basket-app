10612ea4ed8cb4267ed594c8ac1ca83e5f54cbc8
Wizzy-wooz <elena.vodzyanova@gmail.com>
2020-12-27 15:39:36 +0200

    Added description to main class

--- src/main/scala/PriceBasketApp.scala
+++ src/main/scala/PriceBasketApp.scala
@@ -6,6 +6,10 @@ import service.catalog.CatalogReaderJson
 import service.receipt.ReceiptWriterConsole._
 import service.catalog.validation.CatalogVerifier._
 
+/**
+  * App requires arguments to be provided. Check catalog.json for available item names that can be used.
+  *to run application : sbt run Apples Bread Milk Soup Soup
+  */
 object PriceBasketApp extends App{
   val currentCatalog = CatalogReaderJson.getCurrentCatalogFromPath(s"/${ConfigFactory.load().getString("catalogFilePath")}")
   validate(currentCatalog)
